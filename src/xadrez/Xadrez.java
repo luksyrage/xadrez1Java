5c94558cfe49d67b53e1b96fe76d93f0a42658c5
luks <luksy.rage@outlook.com>
2020-10-04 18:49:55 -0300

    Iniciado classes chess e UI

--- src/xadrez/Xadrez.java
+++ src/xadrez/Xadrez.java
@@ -2,11 +2,14 @@ package xadrez;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 
 
 public class Xadrez {
     public static void main(String[] args) {
-        Board board = new Board(8, 8);
+        ChessMatch chessMatch = new ChessMatch();
+        
+        UI.printBoard(chessMatch.getPieces());
         
     }
     
