2034d0fd0955a553ef41a73d07dc9283ecc231e4
luks <luksy.rage@outlook.com>
2020-10-10 16:01:55 -0300

    mostrando cada jogador e turno

--- src/xadrez/UI.java
+++ src/xadrez/UI.java
@@ -1,5 +1,6 @@
 package xadrez;
 
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
@@ -45,7 +46,14 @@ public class UI {
             throw new InputMismatchException("Error reading position, valid values are from a1 to h8");
         }
     } 
-            
+    
+    public static void printMatch (ChessMatch chessMatch){
+        printBoard(chessMatch.getPieces());
+        System.out.println();
+        System.out.println("Turn: " + chessMatch.getTurn());
+        System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+    }
+    
     public static void printBoard(ChessPiece[][] pieces){
         for (int i = 0; i< pieces.length; i++){
             System.out.print((8-i) + " ");
